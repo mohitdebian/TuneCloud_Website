45c84b92b85114a18c27fc14f8fccaf19a7592b9
Mohit || Dev <93594454+mohitdebian@users.noreply.github.com>
2023-12-23 00:16:00 +0530

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import 'focus-visible';
+import { Analytics } from '@vercel/analytics/react';
 import PlausibleProvider from 'next-plausible';
 
 import '@/styles/tailwind.css';
@@ -8,6 +9,7 @@ export default function App({Component, pageProps}) {
   return (
     <PlausibleProvider domain="responsively.app">
       <Component {...pageProps} />
+      <Analytics />
     </PlausibleProvider>
   );
 }
