84b22c58d7367d32dea33b6540db435c55923042
Mohit || Dev <93594454+mohitdebian@users.noreply.github.com>
2023-12-22 08:39:21 +0530

    Add files via upload

---
+++ src/pages/_app.jsx
@@ -0,0 +1,13 @@
+import 'focus-visible';
+import PlausibleProvider from 'next-plausible';
+
+import '@/styles/tailwind.css';
+import '@/styles/carbon-ads.css';
+
+export default function App({Component, pageProps}) {
+  return (
+    <PlausibleProvider domain="responsively.app">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
+}
