8a98cac6e1d745dce28db9034858f5abf5922fbc
Mohit || Dev <93594454+mohitdebian@users.noreply.github.com>
2023-12-22 08:37:39 +0530

    Add files via upload

---
+++ src/components/StarButton/index.tsx
@@ -0,0 +1,26 @@
+import { Icon } from '@iconify/react';
+import { NavLink } from '../NavLink';
+import { useState } from 'react';
+
+export const StarButton = () => {
+  const defaultStarCount = '100+';
+  const [count, setCount] = useState<number>(defaultStarCount);
+
+  return (
+    <div className="group hidden md:block">
+      <NavLink
+        href="https://github.com/mohitdebian"
+        target="_blank"
+      >
+        <span className="flex items-center gap-1">
+          <Icon
+            icon="material-symbols:star-outline-rounded"
+            fontSize={22}
+            className="origin-center transition-transform duration-300 group-hover:scale-125"
+          />
+          {count.toLocaleString()} Stars
+        </span>
+      </NavLink>
+    </div>
+  );
+};
